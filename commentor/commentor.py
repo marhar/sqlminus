0be4ee7b10d962180418ddb040dd619e54549ab6
Mark Harrison <mharrison@lendingclub.com>
2015-11-05 11:38:19 -0800

    applies embedded table comments as comment commands

---
+++ commentor/commentor.py
@@ -0,0 +1,12 @@
+"""
+commentor.py -- pull comments from oracle tables and apply comment cmds
+
+    https://github.com/marhar/sqlminus  :  share and enjoy!
+    Mark Harrison, marhar@gmail.com     :
+"""
+usage_help="""
+usage:
+    commentor connstr sql-files...
+"""
+
+import cx_Oracle
